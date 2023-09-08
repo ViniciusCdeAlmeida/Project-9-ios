663aaf484b4e95d8c887528e16241251bf7c52f3
ViniciusCdeAlmeida <viniciusc.almeida1@gmail.com>
2023-09-08 20:36:53 -0300

    feat: change the anwser

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var falseButton: UIButton!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let questions = [
+        ["Four + Two is equal to Six", "True"],
+        ["Four - Two is equal to Two", "True"],
+        ["Four - One is equal to Two", "False"]
+    ]
+    
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUi()
     }
-
+    
+    @IBAction func anwserButton(_ sender: UIButton) {
+        questionNumber += 1
+        updateUi()
+        if(questionNumber > questions.count){
+            questionNumber = 0
+        }
+    }
+    
+    func updateUi(){
+        questionLabel.text = questions[questionNumber][0]
+        
+    }
+    
 
 }
 
